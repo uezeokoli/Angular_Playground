9e7134fca0f12eff5ec2e4e0b71b4601ebc374b7
Ugo <ugoezeokoli@gmail.com>
2025-05-23 16:39:37 -0700

    add wish feature

--- wishlist/src/app/app.component.ts
+++ wishlist/src/app/app.component.ts
@@ -2,21 +2,26 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   items : WishItem[] = [ 
-    new WishItem('To Learn Angular'),
-    new WishItem('Get Coffee', true),
-    new WishItem('Find grass that cuts itself')
   ]
   title = 'wishlist';
 
+  newWishText = ''
+
+  addNewWish(e : any) {
+    // console.log(this.newWishText)
+    this.items.push(new WishItem(this.newWishText))
+    this.newWishText = ""
+  }
   toggleItem(item : WishItem, e : any ){
     item.isComplete = !item.isComplete
     console.log(`Item is ${item.isComplete ? "":"not "}checked`)

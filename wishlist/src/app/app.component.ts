6e49d63560462b32827f23b1db10abf9003692e4
Ugo <ugoezeokoli@gmail.com>
2025-05-23 17:11:48 -0700

    implemented filter

--- wishlist/src/app/app.component.ts
+++ wishlist/src/app/app.component.ts
@@ -12,19 +12,38 @@ import { FormsModule } from '@angular/forms';
 })
 export class AppComponent {
   items : WishItem[] = [ 
+    new WishItem('To Learn Angular'),
+    new WishItem('Get Coffee', true),
+    new WishItem('Find grass that cuts itself')
   ]
+
   title = 'wishlist';
 
   newWishText = ''
+  listFilter : String = '0'
+
+  visibleItems: WishItem[] = this.items;
 
   addNewWish(e : any) {
     // console.log(this.newWishText)
     this.items.push(new WishItem(this.newWishText))
     this.newWishText = ""
   }
+
+  filterChanged(value : any){
+    if (value == '0'){
+      this.visibleItems = this.items
+    } else if (value == '1') { // unfulfilled wish
+      this.visibleItems = this.items.filter(item => !item.isComplete)
+    } else{//fulfilled wish
+      this.visibleItems = this.items.filter(item => item.isComplete)
+    }
+  }
+
   toggleItem(item : WishItem, e : any ){
     item.isComplete = !item.isComplete
     console.log(`Item is ${item.isComplete ? "":"not "}checked`)
     console.log(e)
   }
 }
+

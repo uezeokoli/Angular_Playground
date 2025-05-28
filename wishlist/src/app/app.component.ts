6ff131456825b426895327a3e1e6402b6762718e
Ugo <ugoezeokoli@gmail.com>
2025-05-27 18:14:01 -0700

    Wish Items are now pulled from a static json using Http request

--- wishlist/src/app/app.component.ts
+++ wishlist/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
 import { CommonModule } from '@angular/common';
@@ -7,6 +7,8 @@ import { WishListComponent } from './wish-list/wish-list.component';
 import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
 import { WishFilterComponent } from './wish-filter/wish-filter.component';
 import {EventService} from '../shared/services/EventService'
+import { WishService } from './wish.service';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -20,16 +22,17 @@ import {EventService} from '../shared/services/EventService'
     WishFilterComponent
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [HttpClient]
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
   items : WishItem[] = [ 
-    new WishItem('To Learn Angular'),
-    new WishItem('Get Coffee', true),
-    new WishItem('Find grass that cuts itself')
+    // new WishItem('To Learn Angular'),
+    // new WishItem('Get Coffee', true),
+    // new WishItem('Find grass that cuts itself')
   ]
 
-  constructor(events : EventService){
+  constructor(events : EventService, private wishService: WishService){
     events.listen('removeWish', (wish : any) => {
 
       // console.log(wish)
@@ -44,5 +47,11 @@ export class AppComponent{
     this.items.push(wish)
   }
 
+  ngOnInit(): void {
+    this.wishService.getWishes().subscribe((data: any) => {
+      this.items = data
+      // console.log(data)
+    })
+  }
 }
 

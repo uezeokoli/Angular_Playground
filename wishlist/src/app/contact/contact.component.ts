47b1e998eb283422aed5aee9eddd61f8d7036a2d
Ugo <ugoezeokoli@gmail.com>
2025-06-03 17:56:39 -0700

    added FormGroup and validators

--- wishlist/src/app/contact/contact.component.ts
+++ wishlist/src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormsModule, ReactiveFormsModule  } from '@angular/forms';
+import { FormGroup, FormControl, FormsModule, ReactiveFormsModule, Validators  } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -16,13 +16,19 @@ export class ContactComponent implements OnInit{
     
   }
 
-  senderNameControl = new FormControl('');
-  senderEmailControl = new FormControl('');
-  senderMessageControl = new FormControl('')
+  contactForm = new FormGroup({
+    senderName: new FormControl('', Validators.required),
+    senderEmail: new FormControl('',[Validators.required, Validators.email]),
+    senderMessage: new FormControl('', [Validators.required, Validators.minLength(10)])
+  })
 
   submitForm(){
-    if(this.senderNameControl.dirty){
-      alert('you changed the name field');
+    // if(this.senderNameControl.dirty){
+    //   alert('you changed the name field');
+    // }
+    console.log(this.contactForm.valid)
+    if (this.contactForm.invalid){
+      alert("Form not valid!")
     }
   }
 
